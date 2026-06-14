08d44e06c230717dc369b8bbcb3ccd4fb927ad5d
Wiktor Wiecheć <wiki3007.ww@gmail.com>
2026-06-14 23:45:49 +0200

    Modified project to integrate Firease Authentication and HotJar with Railway deployment.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import Hotjar from '@hotjar/browser';
 import "./index.css";
 import App from "./App.tsx";
+
+const siteId = 871068; 
+const hotjarVersion = 6;
+Hotjar.init(siteId, hotjarVersion);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
