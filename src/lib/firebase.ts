d9dd2ede34329d5ad98dacaac3d1b31553233b23
Wiktor Wiecheć <wiki3007.ww@gmail.com>
2026-06-16 08:33:40 +0200

    Temporary injected Firebase creditentials to firebase.ts.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 
 // Out web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAMQyzGZhA4Cj5cX6Nm1yFTuFgFeukofUU",
+  authDomain: "cryptoinsighthub-a6687.firebaseapp.com",
+  projectId: "cryptoinsighthub-a6687",
+  storageBucket: "cryptoinsighthub-a6687.firebasestorage.app",
+  messagingSenderId: "172363124115",
+  appId: "1:172363124115:web:ead9f059ea04856bad2c66",
+  measurementId: "G-7253FWLQH2"
 };
 
 console.log("My API key:", import.meta.env.VITE_FIREBASE_API_KEY);
